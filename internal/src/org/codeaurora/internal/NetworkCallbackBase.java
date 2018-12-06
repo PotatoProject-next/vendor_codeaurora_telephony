ba875cfbf2688dfe714408ed28952669d163f98d
Avinash Nalluri <anallu@codeaurora.org>
2018-12-05 19:12:32 -0800

    Remove two way dependency b/w 5G clients and 5G telephony fwk
    Add base class which extends INetworkCallback.Stub and clients
    will use the base class.
    
    Change-Id: I4b59eb1a014def7354efad914a1bcac30f28d735

---
+++ internal/src/org/codeaurora/internal/NetworkCallbackBase.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of The Linux Foundation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.codeaurora.internal;
+
+import android.os.RemoteException;
+import android.util.Log;
+import org.codeaurora.internal.INetworkCallback;
+import org.codeaurora.internal.SignalStrength;
+import org.codeaurora.internal.DcParam;
+import org.codeaurora.internal.Status;
+import org.codeaurora.internal.Token;
+
+public class NetworkCallbackBase extends INetworkCallback.Stub {
+    private static final String TAG = "NetworkCallbackBase";
+
+    @Override
+    public void on5gStatus(int slotId, Token token, Status status, boolean enableStatus) throws
+            RemoteException {
+        Log.d(TAG, "on5gStatus: slotId = " + slotId + " token = " + token + " status" +
+                status + " enableStatus = " + enableStatus);
+    }
+
+    @Override
+    public void onNrDcParam(int slotId, Token token, Status status, DcParam dcParam) throws
+            RemoteException {
+        Log.d(TAG, "onNrDcParam: slotId = " + slotId + " token = " + token + " status" +
+                status + " dcParam = " + dcParam);
+    }
+
+    @Override
+    public void onNrBearerAllocation(int slotId, Token token, Status status, boolean
+            allocated) throws RemoteException {
+        Log.d(TAG,
+                "onNrBearerAllocationChange: slotId = " + slotId + " token = " + token + " " +
+                        "status" + status + " allocated = " + allocated);
+    }
+
+    @Override
+    public void onSignalStrength(int slotId, Token token, Status status, SignalStrength
+            signalStrength) throws RemoteException {
+        Log.d(TAG,
+                "onSignalStrength: slotId = " + slotId + " token = " + token + " " + "status"
+                        + status + " signalStrength = " + signalStrength);
+
+    }
+}
